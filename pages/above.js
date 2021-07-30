195e747164f692407c0acae6bf56ebf7d63a2084
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-07-30 17:45:08 +0530

    Second commit

--- pages/above.js
+++ pages/above.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Head from "next/head";
 
 export default function above() {
     return (
         <div>
+        <Head>
+            <title>Above page</title>
+            <meta />
+        </Head>
             <h1>This is an above page</h1>
         </div>
     )
