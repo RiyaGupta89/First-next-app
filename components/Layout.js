41f8f24d62d2bbea4ecac662b332f75717295bae
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-23 19:09:53 +0530

    Styled navbar

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,15 @@ import Link from "next/link";
 export default function Layout({children}) {
   return (
     <div className={styles.container}>
-      <nav>
+      <nav className={styles.nav}>
+      <div>
+        <span className={styles.logo}>Riya<span className={styles.dot}>.</span></span>
+      </div>
+      <div className={styles.navlinks}>
         <Link href="/">Home</Link>
         <Link href="">Contact</Link>
         <Link href="">Portfolio Projects</Link>
+        </div>
       </nav>
       <main className={styles.main}>
           {children}
